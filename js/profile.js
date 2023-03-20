5d37d77f9eb3e444c38766e1f576b2841cd5947e
KABILDEV-V <119502371+KABILDEV-V@users.noreply.github.com>
2023-03-20 16:46:25 +0530

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         event.preventDefault();
         var form_data = $(this).serialize();
         $.ajax({
-            url: '/php/profile.php',
+            url: 'php/profile.php',
             method: 'POST',
             data: form_data,
             success: function(response) {
